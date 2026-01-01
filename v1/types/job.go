665deae02d4e0629817280d6c73126b283e3afa3
Agu Chijioke <chokey2nv@gmail.com>
2026-01-01 10:51:56 +0100

    job schema

--- v1/types/job.go
+++ v1/types/job.go
@@ -15,16 +15,28 @@ const (
 	Cancelled Status = "CANCELLED"
 )
 
+// type Job struct {
+// 	ID        string
+// 	Type      string
+// 	Status    Status
+// 	Progress  int
+// 	Message   string
+// 	Result    any
+// 	Error     string
+// 	CreatedAt time.Time
+// 	UpdatedAt time.Time
+// }
+
 type Job struct {
-	ID        string
-	Type      string
-	Status    Status
-	Progress  int
-	Message   string
-	Result    any
-	Error     string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID        string    `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty" gql:"required"`
+	Type      string    `protobuf:"bytes,1,opt,name=type,proto3" json:"type,omitempty" gql:"required"`
+	Status    Status    `protobuf:"bytes,1,opt,name=status,proto3" json:"status,omitempty" gql:"required"`
+	Progress  int       `protobuf:"bytes,1,opt,name=progress,proto3" json:"progress,omitempty" gql:"required"`
+	Message   string    `protobuf:"bytes,1,opt,name=message,proto3" json:"message,omitempty" gql:"required"`
+	Result    any       `protobuf:"bytes,1,opt,name=result,proto3" json:"result,omitempty" gql:"required"`
+	Error     string    `protobuf:"bytes,1,opt,name=error,proto3" json:"error,omitempty" gql:"required"`
+	CreatedAt time.Time `protobuf:"bytes,1,opt,name=createdAt,proto3" json:"createdAt,omitempty" gql:"required"`
+	UpdatedAt time.Time `protobuf:"bytes,1,opt,name=updatedAt,proto3" json:"updatedAt,omitempty" gql:"required"`
 }
 
 type JobExecutor func(
